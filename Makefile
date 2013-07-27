6216290dac247dbed7a431a4b1f30ec2d5731ba9
marcinj <junczys@amu.edu.pl>
2013-07-28 01:47:42 +0200

    Removed heavy copying

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: query index
 
 query: query.cpp
-	g++ query.cpp -o query -llucene++ -lpthread -lboost_system -O3
+	g++ query.cpp -o query -llucene++ -lpthread -lboost_system -O6
 
 index: index.cpp
-	g++ index.cpp -o index -llucene++ -lpthread -lboost_system -O3
+	g++ index.cpp -o index -llucene++ -lpthread -lboost_system -O6
 
 clean:
 	rm -rf index query
--- query.cpp
+++ query.cpp
@@ -7,6 +7,7 @@
 
 #include <boost/algorithm/string.hpp>
 #include <boost/foreach.hpp>
+#include <boost/unordered_map.hpp>
 
 #include <lucene++/LuceneHeaders.h>
 #include <lucene++/FileUtils.h>
@@ -23,18 +24,37 @@ typedef std::pair<uint8_t, uint8_t> Span;
 typedef std::vector<Span> SpanSet;
 typedef std::pair<uint32_t, SpanSet> DocSpanSet;
 typedef std::vector<DocSpanSet> Hits;
-typedef std::map<String, Hits> HitCache;
+typedef boost::unordered_map<String, Hits> HitCache;
 
 template <class InputIterator1, class InputIterator2, class OutputIterator>
-  OutputIterator phrase_intersection(InputIterator1 first1, InputIterator1 last1,
+  OutputIterator phrase_join_intersection(InputIterator1 first1, InputIterator1 last1,
                                    InputIterator2 first2, InputIterator2 last2,
                                    OutputIterator result) {
-    while (first1!=last1 && first2!=last2) {
-        if (*first1<*first2) ++first1;
-        else if (*first2<*first1) ++first2;
-        else {  
-            *result = *first1;
-            ++result; ++first1; ++first2;
+    while (first1 != last1 && first2 != last2) {
+        if (first1->first < first2->first)
+            ++first1;
+        else if (first2->first < first1->first)
+            ++first2;
+        else {
+            SpanSet& prefix = first1->second;
+            SpanSet& last   = first2->second;
+            
+            SpanSet adjacent;
+            BOOST_FOREACH(Span s1, prefix) {
+                BOOST_FOREACH(Span s2, last) {
+                    if(s1.second + 1 == s2.first) {
+                        adjacent.push_back(std::make_pair(s1.first, s2.second));
+                        continue;
+                    }
+                }
+            }
+            
+            if(!adjacent.empty()) {
+                *result = DocSpanSet(first1->first, adjacent);
+                ++result;
+            }
+            
+            ++first1; ++first2;
         }
     }
     return result;
@@ -45,14 +65,14 @@ int main(int argc, char** argv) {
     
     String index(_U(argv[1]));
     IndexReaderPtr reader = IndexReader::open(FSDirectory::open(index), true);
-            
-    HitCache cache;
 
+    HitCache cache;
+    
     size_t lines = 0, queries = 0;
     std::string line;
     while (std::getline(std::cin, line))
-    {
-        std::cerr << lines++ << " " << queries << std::endl;
+    {       
+        std::cerr << lines++ << std::endl;
         std::cout << line << std::endl;
         
         String wline(_U(line.c_str()));
@@ -64,56 +84,27 @@ int main(int argc, char** argv) {
                 
         for(size_t i = 0; i < terms.size(); i++) {
             for(size_t l = 1; l <= terms.size() && i + l <= terms.size(); l++) {
+                String prefix;
                 
-                if(l == 1) {
-                    std::cout << "\"" << _B(terms[i]) << "\" : ";
+                Collection<String> phraseTerms = Collection<String>::newInstance();
+                std::wstringstream ss;
+                
+                for(size_t j = 0; j < l; j++) {
+                    if(j != 0)
+                        ss << _U(" ");
                     
-                    if(cache.count(terms[i]) == 0) {
-                        TermPositionsPtr tp = reader->termPositions(
-                            newLucene<Term>(L"source", terms[i]));
-                        while(tp->next()) {
-                            size_t j = 0;
-                            SpanSet spans;
-                            while(j++ < tp->freq()) {
-                                uint32_t pos = tp->nextPosition();
-                                spans.push_back(std::make_pair(pos, pos));
-                            }
-                            cache[terms[i]].push_back(
-                                std::make_pair(tp->doc(), spans));
-                        }
-                    }
-                        
-                    queries++;
+                    ss << terms[i + j];
+                    phraseTerms.add(terms[i + j]);
                     
-                    size_t num = cache[terms[i]].size();
-                    std::cout << num << std::endl;
-                    if(!num)
-                        break;
-                    
+                    if(j == l - 2)
+                        prefix = String(ss.str().c_str());
                 }
-                else {
-                    String prefix;
-                    
-                    Collection<String> phraseTerms = Collection<String>::newInstance();
-                    std::wstringstream ss;
-                    
-                    for(size_t j = 0; j < l; j++) {
-                        if(j != 0)
-                            ss << _U(" ");
-                        
-                        ss << terms[i + j];
-                        phraseTerms.add(terms[i + j]);
-                        
-                        if(j == l - 2)
-                            prefix = String(ss.str().c_str());
-                    }
-                    
-                    String phrase(ss.str().c_str());
-                    
-                    std::cout << "\"" << _B(phrase) << "\" : ";
-                    
+                
+                String phrase(ss.str().c_str());
+                
+                if(cache.count(phrase) == 0) {
                     String last = terms[i + l - 1];
-                    if(cache.count(terms[i]) == 0) {
+                    if(cache.count(last) == 0) {
                         TermPositionsPtr tp = reader->termPositions(
                             newLucene<Term>(L"source", last));
                         while(tp->next()) {
@@ -128,67 +119,51 @@ int main(int argc, char** argv) {
                         }
                     }
                     
-                    Hits hits;
-                    phrase_intersection(cache[last].begin(),
-                                          cache[last].end(),
-                                          cache[prefix].begin(),
-                                          cache[prefix].end(),
-                                          std::back_inserter(hits));
-                    
-                    size_t num = hits.size();
-                    
-                    //std::vector<int> adjacent;
-                    //BOOST_FOREACH(int docId, intersection) {
-                    //    if(docCache.count(docId) == 0)
-                    //        docCache[docId]
-                    //            = boost::static_pointer_cast<TermPositionVector>(
-                    //                reader->getTermFreqVector(docId, L"source"));
-                    //
-                    //    TermPositionVectorPtr tpv = docCache[docId];
-                    //    
-                    //    Collection<int32_t> termIndexes
-                    //        = tpv->indexesOf(phraseTerms, 0, l);
-                    //    
-                    //    Collection<int32_t> starts
-                    //            = tpv->getTermPositions(termIndexes[0]);
-                    //    
-                    //    bool adj = true;
-                    //    //BOOST_FOREACH(uint32_t start, starts) {
-                    //    //    size_t j = 1;
-                    //    //    while(j < l && tpv->getTermPositions(termIndexes[j]).contains(start + j))
-                    //    //        j++;
-                    //    //    if(j == l)
-                    //    //        adj = true;
-                    //    //}
-                    //    
-                    //    if(adj)
-                    //        adjacent.push_back(docId);
-                    //}
-                    
-                    
-                    std::cout << num << std::endl;
-                    if(!num)
-                        break;
-                    
-                    cache[phrase] = hits;
-                    
-                    //BOOST_FOREACH(int docId, adjacent) {
-                    //    DocumentPtr doc = reader->document(docId);
-                    //    SingleString source(_B(doc->get(L"source")));
-                    //    SingleString target(_B(doc->get(L"target")));
-                    //    ByteArray align = doc->getBinaryValue(L"alignment");
-                    //    
-                    //    std::cout << docId << std::endl;
-                    //    std::cout << source << std::endl;
-                    //    std::cout << target << std::endl;
-                    //    size_t i = 0;
-                    //    while(i < align.size()) {
-                    //        std::cout << (size_t)align[i++] << "-";
-                    //        std::cout << (size_t)align[i++] << " ";
-                    //    }
-                    //    std::cout << std::endl;
-                    //}
+                    if(l > 1) {
+                        phrase_join_intersection(cache[prefix].begin(),
+                            cache[prefix].end(),
+                            cache[last].begin(),
+                            cache[last].end(),
+                            std::back_inserter(cache[phrase]));
+                    }    
                 }
+                
+                std::cout << "\"" << _B(phrase) << "\" : ";
+                size_t num = cache[phrase].size();
+                std::cout << num << std::endl;  
+                
+                if(!num)
+                    break;
+                
+                Hits nhits;
+                size_t n = 10;
+                
+                Hits& hits = cache[phrase];
+                //std::random_shuffle(hits.begin(), hits.end());
+                std::copy(hits.begin(), hits.begin() + (n < hits.size() ? n
+                                                        : hits.size()),
+                          std::back_inserter(nhits));
+                
+                BOOST_FOREACH(DocSpanSet docSpanSet, nhits) {
+                    int docId = docSpanSet.first;
+                    DocumentPtr doc = reader->document(docId);
+                    SingleString source(_B(doc->get(L"source")));
+                    SingleString target(_B(doc->get(L"target")));
+                    ByteArray align = doc->getBinaryValue(L"alignment");
+                    
+                    BOOST_FOREACH(Span s, docSpanSet.second)
+                        std::cout << docId << " [" << (size_t)s.first << ","
+                        << (size_t)s.second << "]" << std::endl;
+                    std::cout << source << std::endl;
+                    std::cout << target << std::endl;
+                    size_t i = 0;
+                    while(i < align.size()) {
+                        std::cout << (size_t)align[i++] << "-";
+                        std::cout << (size_t)align[i++] << " ";
+                    }
+                    std::cout << std::endl;
+                }
+                std::cout << std::endl;
             }
         }
         std::cout << std::endl;
